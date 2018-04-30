9aa50db49252de528c8b0650b2966223553f2e64
averude <andreykol46@yahoo.com>
2018-04-30 13:21:38 +0300

    Deleted commentary

--- src/main/java/dao/GenericDAO.java
+++ src/main/java/dao/GenericDAO.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 public interface GenericDAO<T extends Serializable> {
     Collection<T> findAll();
     T findById(final long id);
-    // CRUD
     void update(final T t);
     void delete(final T t);
     void create(final T t);
