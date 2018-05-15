95e1dd0f314d47d676645e14c26e95b3e35c3daf
averude <andreykol46@yahoo.com>
2018-05-15 15:40:23 +0300

    Added JUnit 4.12 and some tests

--- src/test/java/entity/EmployeeTest.java
+++ src/test/java/entity/EmployeeTest.java
@@ -1,33 +1,39 @@
 package entity;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@RunWith(JUnitParamsRunner.class)
 public class EmployeeTest {
 
-    private String firstName = "Charles";
-    private String secondName = "Bronks";
-    private Long posId = 1L;
-    private Long shId = 1L;
-    private Long id = 1L;
+    private static final Object[] getEmployees() {
+        return new Object[] {
+                new Object[] {"Charles", "Bronks", 1L, 1L, 1L},
+                new Object[] {"Frank", "Lampard", 10L, 269L, 589L},
+                new Object[] {"Richard", "Hopkins", -1L, -1L, -1L},
+                new Object[] {"Tobby", "Bryan", 123L, 145L, 23L},
+                new Object[] {"Castor", "Troy", 12L, 123L, 546L}
+        };
+    }
 
-    private Employee employee = new Employee();
-
-    @BeforeAll
-    void initTest(){
+    @Test
+    @Parameters(method = "getEmployees")
+    public void testSuccessEmployeeFields(String firstName,
+                                   String secondName,
+                                   Long posId,
+                                   Long shId,
+                                   Long id){
+        Employee employee = new Employee();
         employee.setId(id);
         employee.setFirstName(firstName);
         employee.setSecondName(secondName);
         employee.setPositionId(posId);
         employee.setShiftId(shId);
-    }
 
-    @Test
-    void testSuccessEmployeeFields(){
         assertEquals(id, employee.getId());
         assertEquals(firstName, employee.getFirstName());
         assertEquals(secondName, employee.getSecondName());
@@ -36,16 +42,17 @@ public class EmployeeTest {
     }
 
     @Test
-    void testSuccessEmployeeCollection(){
-        Schedule s = new Schedule();
+    public void testSuccessEmployeeCollection(){
+        Employee employee = new Employee();
+        Schedule schedule = new Schedule();
 
-        employee.addSchedule(s);
+        employee.addSchedule(schedule);
         assertEquals(1, employee.getSchedule().size());
-        assertTrue(employee.getSchedule().contains(s));
+        assertTrue(employee.getSchedule().contains(schedule));
 
-        employee.removeSchedule(s);
+        employee.removeSchedule(schedule);
         assertEquals(0, employee.getSchedule().size());
-        assertFalse(employee.getSchedule().contains(s));
+        assertFalse(employee.getSchedule().contains(schedule));
     }
 
     // Validation tests
