13eec8defde2e52a5316bcafe0826deedf7b2534
averude <andreykol46@yahoo.com>
2018-10-24 10:39:36 +0300

    Added new components

--- src/app/modules/client/client.module.ts
+++ src/app/modules/client/client.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
-import { ClientComponent } from './client.component';
 import { ClientRoutingModule } from './client-routing.module';
+import { CommonModule } from '@angular/common';
+import { ScheduleTableComponent } from './components/schedule-table/schedule-table.component';
+import { ScheduleTablePaginatorComponent } from './components/schedule-table-paginator/schedule-table-paginator.component';
+import { PaginatorService } from './paginator.service';
+import { ScheduleTableCellComponent } from './components/schedule-table-cell/schedule-table-cell.component';
 
 @NgModule({
-  imports: [ClientRoutingModule],
-  declarations: [ClientComponent]
+  imports: [CommonModule, ClientRoutingModule],
+  declarations: [
+    ScheduleTableComponent,
+    ScheduleTablePaginatorComponent,
+    ScheduleTableCellComponent
+  ],
+  providers: [PaginatorService]
 })
 export class ClientModule {}
