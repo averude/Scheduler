14bf90f882a5ec0e647a2ccbd4822dbe60d6c4a6
averude <andreykol46@yahoo.com>
2018-12-28 10:59:08 +0200

    Code refactoring

--- scheduler-rest-server/src/main/java/dao/DayTypeDAO.java
+++ scheduler-rest-server/src/main/java/dao/DayTypeDAO.java
@@ -2,8 +2,6 @@ package dao;
 
 import entity.DayType;
 
-import java.util.List;
-
 public interface DayTypeDAO extends GenericDAO<DayType> {
-    List<DayType> findByEmployeeId(long employeeId);
+
 }
