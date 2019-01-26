69a20c22cd7446af575abdb5f9df935e09117d88
averude <andreykol46@yahoo.com>
2019-01-26 16:15:58 +0200

    Moved to 'services' folder

--- src/app/modules/admin/management/components/patterns/services/pattern-switch.service.ts
+++ src/app/modules/admin/management/components/patterns/services/pattern-switch.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { PatternUnit } from '../../../../../model/patternunit';
+import { PatternUnit } from '../../../../../../model/patternunit';
 
 @Injectable()
 export class PatternSwitchService {
