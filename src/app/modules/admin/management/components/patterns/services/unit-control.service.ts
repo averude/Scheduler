34d62d221288113eef906102ac92685d5c5844c3
averude <andreykol46@yahoo.com>
2019-01-26 16:09:56 +0200

    Initial commit

---
+++ src/app/modules/admin/management/components/patterns/services/unit-control.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {Observable, Subject} from "rxjs";
+import {PatternUnit} from "../../../../../../model/patternunit";
+
+@Injectable()
+export class UnitControlService {
+
+  private up:   Subject<PatternUnit> = new Subject<PatternUnit>();
+  private down: Subject<PatternUnit> = new Subject<PatternUnit>();
+  private del:  Subject<PatternUnit> = new Subject<PatternUnit>();
+
+  constructor() { }
+
+  moveUp(patternUnit: PatternUnit) {
+    this.up.next(patternUnit);
+  }
+
+  moveDown(patternUnit: PatternUnit) {
+    this.down.next(patternUnit);
+  }
+
+  delete(patternUnit: PatternUnit) {
+    this.del.next(patternUnit);
+  }
+
+  getMoveUp(): Observable<PatternUnit> {
+    return this.up.asObservable();
+  }
+
+  getMoveDown(): Observable<PatternUnit> {
+    return this.down.asObservable();
+  }
+
+  getDelete(): Observable<PatternUnit> {
+    return this.del.asObservable();
+  }
+}
