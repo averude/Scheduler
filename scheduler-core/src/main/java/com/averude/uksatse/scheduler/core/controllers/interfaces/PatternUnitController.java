54f40320b00fce649ae78b693b0b91f6745d41ad
averude <andreykol46@yahoo.com>
2019-02-23 12:27:16 +0200

    Changed the REST URI of pattern units' controller

--- scheduler-core/src/main/java/com/averude/uksatse/scheduler/core/controllers/interfaces/PatternUnitController.java
+++ scheduler-core/src/main/java/com/averude/uksatse/scheduler/core/controllers/interfaces/PatternUnitController.java
@@ -7,33 +7,30 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Optional;
 
-@RequestMapping("/patterns/{patternId}/units")
 public interface PatternUnitController {
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET,
+                    value = "/patterns/{patternId}/units")
     Iterable<PatternUnit> getAll(@RequestHeader("Department-ID") Long departmentId,
                                  @PathVariable Long patternId);
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST,
+                    value = "/units")
     ResponseEntity<?> create(@RequestHeader("Department-ID") Long departmentId,
-                             @PathVariable Long patternId,
                              @Valid @RequestBody PatternUnit unit);
 
     @RequestMapping(method = RequestMethod.GET,
-                    value = "{unitId}")
+                    value = "/units/{unitId}")
     Optional<PatternUnit> get(@RequestHeader("Department-ID") Long departmentId,
-                              @PathVariable Long patternId,
                               @PathVariable Long unitId);
 
     @RequestMapping(method = RequestMethod.PUT,
-                    value = "{unitId}")
+                    value = "/units/{unitId}")
     ResponseEntity<?> update(@RequestHeader("Department-ID") Long departmentId,
-                             @PathVariable Long patternId,
                              @PathVariable Long unitId,
                              @Valid @RequestBody PatternUnit unit);
 
     @RequestMapping(method = RequestMethod.DELETE,
-                    value = "{unitId}")
+                    value = "/units/{unitId}")
     ResponseEntity<?> delete(@RequestHeader("Department-ID") Long departmentId,
-                             @PathVariable Long patternId,
                              @PathVariable Long unitId);
 }
