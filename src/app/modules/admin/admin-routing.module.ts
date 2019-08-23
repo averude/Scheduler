495911527e098b69b60c0a89721a9f158aacdfcf
averude <andreykol46@yahoo.com>
2019-08-23 22:39:33 +0300

    Implemented lazy loading modules

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -3,64 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { SchedulesTableComponent } from './calendar/components/schedules-table/schedules-table.component';
 import { AdminComponent } from './admin/admin.component';
 import { RoleGuard } from '../../guards/role-guard.service';
-import { ManagementComponent } from './management/management/management.component';
-import { EmployeesTableComponent } from "./management/components/employees/components/employees-table/employees-table.component";
-import { PositionsTableComponent } from "./management/components/positions/components/positions-table/positions-table.component";
-import { DayTypesTableComponent } from "./management/components/daytypes/components/daytypes-table/daytypes-table.component";
-import { ShiftsTableComponent } from "./management/components/shifts/components/shifts-table/shifts-table.component";
-import { PatternsTableComponent } from "./management/components/patterns/components/patterns-table/patterns-table.component";
-import { HolidaysTableComponent } from "./management/components/holidays/components/holidays-table/holidays-table.component";
-import { WorkingTimeTableComponent } from "./management/components/working-time/components/working-time-table/working-time-table.component";
-import { ExtraWeekendsTableComponent } from "./management/components/extra-weekends/components/extra-weekends-table/extra-weekends-table.component";
 import { StatisticsTableComponent } from "./statistics/components/statistics-table/statistics-table.component";
 import { ScheduleTabBarComponent } from "./schedule-tab-bar/schedule-tab-bar.component";
 
 const routes: Routes = [
   {
-    path: 'admin',
+    path: '',
     component: AdminComponent,
-    canActivate: [RoleGuard],
-    data: {roles: ['DEPARTMENT_ADMIN', 'SHIFT_ADMIN']},
     children: [
       {
         path: 'management',
-        component: ManagementComponent,
         canActivate: [RoleGuard],
         data: {roles: ['DEPARTMENT_ADMIN']},
-        children: [
-          {
-            path: 'employees',
-            component: EmployeesTableComponent
-          },
-          {
-            path: 'positions',
-            component: PositionsTableComponent
-          },
-          {
-            path: 'daytypes',
-            component: DayTypesTableComponent
-          },
-          {
-            path: 'patterns',
-            component: PatternsTableComponent
-          },
-          {
-            path: 'shifts',
-            component: ShiftsTableComponent
-          },
-          {
-            path: 'holidays',
-            component: HolidaysTableComponent
-          },
-          {
-            path: 'workingtime',
-            component: WorkingTimeTableComponent
-          },
-          {
-            path: 'extraweekends',
-            component: ExtraWeekendsTableComponent
-          }
-        ]
+        loadChildren: () => import('./management/management.module')
+          .then(mod => mod.ManagementModule)
       },
       {
         path: 'schedule',
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,24 +3,22 @@ import { SchedulesModule } from './calendar/schedules.module';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin/admin.component';
 import { AdminRoutingModule } from './admin-routing.module';
-import { ManagementModule } from './management/management.module';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SimpleNotificationsModule } from "angular2-notifications";
-import { OPTIONS } from "./notification-options";
 import { StatisticsModule } from "./statistics/statistics.module";
 import { ScheduleTabBarComponent } from './schedule-tab-bar/schedule-tab-bar.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ManagementModule,
     SchedulesModule,
     StatisticsModule,
     AdminRoutingModule,
-    BrowserAnimationsModule,
-    SimpleNotificationsModule.forRoot(OPTIONS)
+    SimpleNotificationsModule
   ],
-  declarations: [AdminComponent, ScheduleTabBarComponent],
+  declarations: [
+    AdminComponent,
+    ScheduleTabBarComponent
+  ],
   exports: [],
 })
 export class AdminModule {}
