ee5186c3521f56a04a659f531bf24c4aabd54aa3
averude <andreykol46@yahoo.com>
2020-05-29 21:48:14 +0300

    Added enterprise entity and moved day types owner to enterprise. For departments created department day type

---
+++ scheduler-frontend-server/src/app/modules/global_admin/enterprise-admin-user-accounts/components/user-accounts-dialog/user-accounts-dialog.component.ts
@@ -0,0 +1,49 @@
+import { Component, Inject } from '@angular/core';
+import { DialogBaseComponent } from "../../../../../shared/abstract-components/dialog-base/dialog-base.component";
+import { EnterpriseAdminUserAccount, UserAccount } from "../../../../../model/accounts/user-account";
+import { FormBuilder, Validators } from "@angular/forms";
+import { AuthService } from "../../../../../http-services/auth.service";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { Enterprise } from "../../../../../model/enterprise";
+
+@Component({
+  selector: 'app-user-accounts-dialog',
+  templateUrl: './user-accounts-dialog.component.html',
+  styleUrls: ['../../../../../shared/common/dialog.common.css', './user-accounts-dialog.component.css']
+})
+export class UserAccountsDialogComponent extends DialogBaseComponent<UserAccount> {
+
+  enterprises:  Enterprise[]  = [];
+
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private dialogRef: MatDialogRef<UserAccountsDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) data) {
+    super(data.userAccount, dialogRef);
+    this.enterprises  = data.enterprises;
+  }
+
+  initTheForm() {
+    this.dialogForm = this.fb.group({
+      username:     ['', [Validators.required,
+                          Validators.minLength(3),
+                          Validators.maxLength(128)]],
+      password:     ['', [Validators.required,
+                          Validators.minLength(3),
+                          Validators.maxLength(128)]],
+      enterpriseId: [null,  [Validators.required]],
+      locked:       [false, [Validators.required]],
+      enabled:      [true,  [Validators.required]]
+    });
+  }
+
+  fillInTheForm(userAccount: EnterpriseAdminUserAccount) {
+    this.dialogForm.setValue({
+      username:     userAccount.username,
+      password:     null,
+      enterpriseId: userAccount.enterpriseId,
+      locked:       userAccount.locked,
+      enabled:      userAccount.enabled
+    });
+  }
+}
