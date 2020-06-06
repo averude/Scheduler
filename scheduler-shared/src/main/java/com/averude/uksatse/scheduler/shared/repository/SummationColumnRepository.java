1df8d2b24cdee0e086f00c235e32798abbbacc92
averude <andreykol46@yahoo.com>
2020-06-06 15:05:39 +0300

    Added an ability to find summation columns by department and shift id

--- scheduler-shared/src/main/java/com/averude/uksatse/scheduler/shared/repository/SummationColumnRepository.java
+++ scheduler-shared/src/main/java/com/averude/uksatse/scheduler/shared/repository/SummationColumnRepository.java
@@ -2,6 +2,24 @@ package com.averude.uksatse.scheduler.shared.repository;
 
 import com.averude.uksatse.scheduler.core.entity.SummationColumn;
 import com.averude.uksatse.scheduler.shared.repository.interfaces.IByEnterpriseIdRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface SummationColumnRepository extends IByEnterpriseIdRepository<SummationColumn, Long> {
+import java.util.List;
+
+public interface SummationColumnRepository
+        extends IByEnterpriseIdRepository<SummationColumn, Long> {
+
+    @Query("select sc from SummationColumn as sc " +
+            "inner join Department as d " +
+            "on sc.enterpriseId = d.enterpriseId " +
+            "where d.id = ?1")
+    List<SummationColumn> findAllByDepartmentId(Long departmentId);
+
+    @Query("select sc from SummationColumn as sc " +
+            "inner join Department as d " +
+            "on sc.enterpriseId = d.enterpriseId " +
+            "inner join Shift as s " +
+            "on d.id = s.departmentId " +
+            "where s.id = ?1")
+    List<SummationColumn> findAllByShiftId(Long shiftId);
 }
