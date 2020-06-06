eaf037a5e6c754ec3ff19ce6f122ea41b83af0e4
averude <andreykol46@yahoo.com>
2020-06-06 15:07:13 +0300

    Added validation annotation to the transfer method

--- scheduler-shared/src/main/java/com/averude/uksatse/scheduler/shared/service/ExtraWeekendService.java
+++ scheduler-shared/src/main/java/com/averude/uksatse/scheduler/shared/service/ExtraWeekendService.java
@@ -7,10 +7,11 @@ import com.averude.uksatse.scheduler.core.service.IByEnterpriseIdAndDateService;
 import com.averude.uksatse.scheduler.core.service.IByShiftIdAndDateService;
 import com.averude.uksatse.scheduler.core.service.IService;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 
 public interface ExtraWeekendService extends IByEnterpriseIdAndDateService<ExtraWeekend, Long>,
         IByDepartmentIdAndDateService<ExtraWeekend, Long>, IByShiftIdAndDateService<ExtraWeekend, Long>,
         IService<ExtraWeekend, Long> {
-    ExtraWorkDay transferWorkDay(ExtraWeekend extraWeekend, LocalDate date);
+    ExtraWorkDay transferWorkDay(@Valid ExtraWeekend extraWeekend, LocalDate date);
 }
