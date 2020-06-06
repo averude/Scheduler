45249714094421fa789883195e79818aa5104e7b
averude <andreykol46@yahoo.com>
2020-06-06 15:07:54 +0300

    Changed propagation of transaction

--- scheduler-shared/src/main/java/com/averude/uksatse/scheduler/shared/creator/ExtraDayCreatorImpl.java
+++ scheduler-shared/src/main/java/com/averude/uksatse/scheduler/shared/creator/ExtraDayCreatorImpl.java
@@ -7,6 +7,8 @@ import com.averude.uksatse.scheduler.shared.repository.ExtraWeekendRepository;
 import com.averude.uksatse.scheduler.shared.repository.HolidayRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -26,6 +28,7 @@ public class ExtraDayCreatorImpl implements ExtraDayCreator {
     }
 
     @Override
+    @Transactional(propagation = Propagation.MANDATORY)
     public ExtraWeekend createExtraWeekend(Holiday holiday) {
         var nextWeekFirstDay = getNextWeekDayDate(holiday.getDate(), DayOfWeek.MONDAY);
 
@@ -38,7 +41,6 @@ public class ExtraDayCreatorImpl implements ExtraDayCreator {
                         .map(existentHoliday -> {
                             var dayOfWeek = existentHoliday.getDate().getDayOfWeek();
                             if (dayOfWeek.compareTo(DayOfWeek.SUNDAY) == 0) {
-                                // Should be changed because of opened session
                                 weekend.setDate(weekend.getDate().plusDays(1L));
                                 return createNewExtraWeekend(holiday, nextWeekFirstDay);
                             } else {
