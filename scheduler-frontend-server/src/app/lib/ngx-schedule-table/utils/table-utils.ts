337bf976a2f0453c54f716ea099b0eec624d7f71
averude <andreykol46@yahoo.com>
2020-07-12 18:06:02 +0300

    Fixed issue when cell wasn't set disabled label

--- scheduler-frontend-server/src/app/lib/ngx-schedule-table/utils/table-utils.ts
+++
@@ -1,5 +0,0 @@
-import * as moment from "moment";
-
-export function isBetween(value: string, from: string, to: string): boolean {
-  return moment(value).isBetween(moment(from), moment(to),'date', '[]');
-}
