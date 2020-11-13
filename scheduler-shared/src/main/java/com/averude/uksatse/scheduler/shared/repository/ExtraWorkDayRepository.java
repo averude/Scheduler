9450ca7ac65e2db4972c7a85dd07cbfb39c78bf7
averude <andreykol46@yahoo.com>
2020-11-13 18:26:12 +0200

    Removed unused entities, repositories, services and controllers

--- scheduler-shared/src/main/java/com/averude/uksatse/scheduler/shared/repository/ExtraWorkDayRepository.java
+++
@@ -1,7 +0,0 @@
-package com.averude.uksatse.scheduler.shared.repository;
-
-import com.averude.uksatse.scheduler.core.entity.ExtraWorkDay;
-import com.averude.uksatse.scheduler.shared.repository.interfaces.IByEnterpriseIdAndDateRepository;
-
-public interface ExtraWorkDayRepository extends IByEnterpriseIdAndDateRepository<ExtraWorkDay, Long> {
-}
