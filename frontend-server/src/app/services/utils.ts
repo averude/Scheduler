34c535ab0f671afe6ef8908d9e880e70c89ac3a0
averude <andreykol46@yahoo.com>
2021-07-07 12:25:22 +0300

    Fixed issue with the latest shift determination

--- frontend-server/src/app/services/utils.ts
+++ frontend-server/src/app/services/utils.ts
@@ -2,17 +2,29 @@ import { EmployeeScheduleDTO } from "../model/dto/employee-schedule-dto";
 
 export function getMainShiftId(dto: EmployeeScheduleDTO) {
 
-  const lastMainCompositionIndex = dto.mainCompositions.length - 1;
-
-  if (lastMainCompositionIndex >= 0) {
-    return dto.mainCompositions[lastMainCompositionIndex].shiftId;
+  if (dto.mainCompositions.length > 0) {
+    return findMax(dto.mainCompositions, (prev, curr) => prev.from.isBefore(curr.from)).shiftId;
   } else {
-    const lastSubstitutionCompositionIndex = dto.substitutionCompositions.length - 1;
-
-    if (lastSubstitutionCompositionIndex >= 0) {
-      return dto.substitutionCompositions[lastSubstitutionCompositionIndex]
-        .mainComposition
-        .shiftId;
+    if (dto.substitutionCompositions.length > 0) {
+      return findMax(dto.substitutionCompositions, (prev, curr) => prev.from.isBefore(curr.from)).shiftId;
     }
   }
+}
+
+export function findMax<T>(arr: T[],
+                           comparator: (prev: T, curr: T) => boolean): T {
+  if (arr.length == 0) {
+    return undefined;
+  }
+
+  let maxVal = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    const val = arr[i];
+    if (comparator(maxVal, val)) {
+      maxVal = val;
+    }
+  }
+
+  return maxVal;
 }
