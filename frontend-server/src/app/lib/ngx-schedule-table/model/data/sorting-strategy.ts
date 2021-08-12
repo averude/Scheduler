f5d11435c31ed9a3367efcb8dd904ff37f0f1a2a
averude <andreykol46@yahoo.com>
2021-08-12 15:14:47 +0300

    Code refactoring

--- frontend-server/src/app/lib/ngx-schedule-table/model/data/sorting-strategy.ts
+++ frontend-server/src/app/lib/ngx-schedule-table/model/data/sorting-strategy.ts
@@ -3,19 +3,3 @@ import { RowGroup } from "./row-group";
 export interface SortingStrategy<T extends RowGroup> {
   sort(groups: T[]): T[];
 }
-
-class SimpleSortingStrategy implements SortingStrategy<RowGroup> {
-
-  sort(groups: RowGroup[]): RowGroup[] {
-    if (!groups || groups.length === 0) {
-      return groups;
-    }
-
-    let res: RowGroup[] = [].concat(groups);
-    res.sort((a, b) => b.id - a.id);
-    return res;
-  }
-
-}
-
-export const SIMPLE_SORTING_STRATEGY: SortingStrategy<RowGroup> = new SimpleSortingStrategy();
