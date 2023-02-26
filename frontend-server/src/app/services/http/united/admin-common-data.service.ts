2d83e25af16df1afd68f7d6b625e8b28b651c3ea
averude <andreykol46@yahoo.com>
2023-02-26 14:17:07 +0200

    Integrated GET common and admin common data endpoints

--- frontend-server/src/app/services/http/united/admin-common-data.service.ts
+++ frontend-server/src/app/services/http/united/admin-common-data.service.ts
@@ -1,10 +1,10 @@
 import { forkJoin, Observable, of } from "rxjs";
 import { AdminCommonDataDTO } from "../../../model/dto/united/admin-common-data-dto";
 import { RatioColumnService } from "../ratio-column.service";
-import { ShiftPatternDtoService } from "../shift-pattern-dto.service";
-import { DepartmentDayTypeService } from "../department-day-type.service";
 import { map } from "rxjs/operators";
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { RestConfig } from "../../../rest.config";
 
 export const DEPARTMENTS: number[] = [11, 16, 17, 18, 19, 20];
 
@@ -13,11 +13,10 @@ export const DEPARTMENTS: number[] = [11, 16, 17, 18, 19, 20];
 })
 export class AdminCommonDataService {
 
-  constructor(private ratioColumnService: RatioColumnService,
-              private shiftPatternDtoService: ShiftPatternDtoService,
-              private departmentDayTypeService: DepartmentDayTypeService) {}
+  constructor(private httpClient: HttpClient,
+              private config: RestConfig,
+              private ratioColumnService: RatioColumnService) {}
 
-  // TODO: exchange with backend endpoint
   getByEnterpriseAndDepartmentId(enterpriseId: number,
                                  departmentId: number): Observable<AdminCommonDataDTO> {
     const obs: Observable<any>[] = [
@@ -25,16 +24,16 @@ export class AdminCommonDataService {
       DEPARTMENTS
         .indexOf(departmentId) >= 0 ? this.ratioColumnService.getAllByEnterpriseId(enterpriseId) : of([]),
       //
-      this.shiftPatternDtoService.getAllByDepartmentId(departmentId),
-      this.departmentDayTypeService.getAllByDepartmentId(departmentId)
+      this.httpClient.get<AdminCommonDataDTO>(
+        `${this.config.baseUrl}/enterprises/${enterpriseId}/departments/${departmentId}/admin_common_data`
+      )
     ];
 
     return forkJoin(obs).pipe(
-      map(([ratioColumns, patternDTOs, departmentDayTypes]) => ({
-        ratioColumns: ratioColumns,
-        patternDTOs: patternDTOs,
-        departmentDayTypes: departmentDayTypes
-      }))
+      map(([ratioColumns, adminCommonData]) => {
+        adminCommonData.ratioColumns = ratioColumns;
+        return adminCommonData;
+      })
     )
   }
 }
--- frontend-server/src/app/services/http/united/common-data.service.ts
+++ frontend-server/src/app/services/http/united/common-data.service.ts
@@ -1,43 +1,22 @@
-import { forkJoin, Observable } from "rxjs";
+import { Observable } from "rxjs";
 import { CommonDataDTO } from "../../../model/dto/united/common-data-dto";
 import { Injectable } from "@angular/core";
-import { DayTypeService } from "../day-type.service";
-import { PositionService } from "../position.service";
-import { ShiftService } from "../shift.service";
-import { EmployeeService } from "../employee.service";
-import { map } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { RestConfig } from "../../../rest.config";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonDataService {
 
-  constructor(private dayTypeService: DayTypeService,
-              private positionService: PositionService,
-              private shiftService: ShiftService,
-              private employeeService: EmployeeService){
+  constructor(private httpClient: HttpClient,
+              private config: RestConfig){
   }
-
-  // TODO: should be exchanged with the new endpoint on the backend side
+  
   getByEnterpriseIdAndDepartmentId(enterpriseId: number,
                                    departmentId: number): Observable<CommonDataDTO> {
-    const sources: Observable<any>[] = [
-      this.dayTypeService.getAllByEnterpriseId(enterpriseId),
-      this.positionService.getAllByDepartmentId(departmentId),
-      this.shiftService.getAllByDepartmentId(departmentId),
-      this.employeeService.getAllByDepartmentId(departmentId)
-    ];
-
-    return forkJoin(sources)
-      .pipe(
-        map(([dayTypes, positions, shifts, employees]) => {
-          return {
-            dayTypes:   dayTypes,
-            positions:  positions,
-            shifts:     shifts,
-            employees:  employees
-          }
-        })
-      );
+    return this.httpClient.get<CommonDataDTO>(
+      `${this.config.baseUrl}/enterprises/${enterpriseId}/departments/${departmentId}/common_data`
+    );
   }
 }
